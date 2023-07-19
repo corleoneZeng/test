96b695b4db5c498bb3bb5a14be2491db9d2ad6b2
CorleoneZeng <corleone.zeng@klook.com>
2023-07-19 10:39:56 +0800

    upload main.go

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Git is a distributed version control system")
 	fmt.Println("Git is free software distributed under the GPL")
+	sum := add(1, 2)
+	fmt.Println("sum=", sum)
+}
+
+func add(a, b int) int {
+	return a + b
 }
